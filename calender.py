e65ab26d69d6d4895319360b01eba01ea6a3402e
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-05 23:26:04 +0530

    Calendar using Module

---
+++ calender.py
@@ -0,0 +1,4 @@
+import calendar
+yy = int(input("Enter The Year : "))     #Input year in 4 digit format.
+mm = int(input("Enter The Month : "))    #Input Month Number Which you want calendar.
+print(calendar.month(yy,mm))
